45ccf00966d04cf6e88c4569e60e0a75ce7202a1
Russell Dermody <russ.dermody@gmail.com>
2019-10-08 12:34:23 -0700

    Deployment Changes

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const logger = require('morgan')
+const express = require("express");
+const logger = require("morgan");
 // * dotenv
-require('dotenv').config()
+require("dotenv").config();
+// *Githil Pages Deploy
+var ghpages = require("gh-pages");
+ghpages.publish("RED-Portfolio-Page", function(err) {});
 
-var PORT = process.env.PORT
+var PORT = process.env.PORT;
 
 const app = express();
 // *Using morgan for logging
-app.use(logger("dev"))
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // * Make 'public' a static folder
 app.use(express.static("public"));
 
 // Start the server
-app.listen(PORT, function () {
-    console.log(`Listening on PORT: ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`Listening on PORT: ${PORT}`);
 });
